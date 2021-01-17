8a6a4d5b68599aa5c3afaf18e1c77146a61837ad
Mohamed Ahmed <moh.ahmed.abass@gmail.com>
2021-01-18 01:15:52 +0200

    user profile and picture done

--- django_project/urls.py
+++ django_project/urls.py
@@ -24,5 +24,9 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.profile, name='profile'),
     path('',include('blog.urls')),
-
 ]
+
+from django.conf.urls.static import static
+from django.conf import settings
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
