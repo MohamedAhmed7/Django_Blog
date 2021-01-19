2b097d3d19c0a9e0b4fd97e61263bde15df644a1
Mohamed Ahmed <moh.ahmed.abass@gmail.com>
2021-01-19 18:03:07 +0200

    list all the posts and replies ordered from newest to oldest

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about')
+    path('', views.PostListView.as_view(), name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail' ),
+#path('post/<int:pk>/', views.RepliesListView.as_view(), name='post-replies' ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Post
+from django.views.generic import ListView, DetailView
+from .models import Post, Reply
 # Create your views here.
 
 def home(request):
@@ -8,6 +9,22 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted'] # order from newest to oldest
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['replies'] = Reply.objects.filter(post = context['post']).all()
+        return context
 
 def about(request):
     return render(request, 'blog/about.html')
