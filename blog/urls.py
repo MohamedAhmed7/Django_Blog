df7b0806a70b1976d06cb9124343a033295268b8
Mohamed Ahmed <moh.ahmed.abass@gmail.com>
2021-01-19 20:05:16 +0200

    user posts create update and delete with authentication

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-new'),
+    path('post/<int:pk>/update', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', views.PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail' ),
-#path('post/<int:pk>/', views.RepliesListView.as_view(), name='post-replies' ),
 ]
